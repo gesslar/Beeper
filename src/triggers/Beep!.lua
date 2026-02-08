41aa6143f2a969d7d5f3ecbeb1a37fed411c5a51
gesslar <1266935+gesslar@users.noreply.github.com>
2026-02-08 02:07:30 -0500

    deprecation, but also some cleanliness

--- src/triggers/Beep!.lua
+++ src/triggers/Beep!.lua
@@ -1,6 +1,6 @@
 local dir = getMudletHomeDir() .. "/__PKGNAME__/"
 local ThresholdBeepFile = dir .. "Media/Sounds/91926__tim-kahn__ding.wav"
-print(ThresholdBeepFile)
+
 playSoundFile(ThresholdBeepFile)
 
 for _, match in pairs(matches) do
